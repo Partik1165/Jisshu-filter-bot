af8d7d8ad1955ed019bb0e3cfb2aad9e448e764a
Umesh Saini <umeshsingodiya26@gmail.com>
2025-07-21 12:42:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/JisshuTG/Jisshu-filter-bot /Jisshu-filter-bot
+  git clone https://github.com/Partik1165/Jisshu-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Jisshu-filter-bot
