9389a4eed67570cee4f7f5a932db4a7fb859933a
Umesh Saini <umeshsingodiya26@gmail.com>
2025-09-16 15:15:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/partik1165/Jisshu-filter-bot /Jisshu-filter-bot
+  git clone https://github.com/partik1165/UMESH-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Jisshu-filter-bot
+  git clone $UPSTREAM_REPO /UMESH-BOT
 fi
 cd /Jisshu-filter-bot
 pip3 install -U -r requirements.txt
