49b9a683ba14939117b11e8e043cf5c7d40f5bd8
Umesh Saini <umeshsingodiya26@gmail.com>
2025-09-16 14:38:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Partik1165/UMESH-BOT /UMESH-BOT
+  git clone https://github.com/Partik1165/UMESH-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /UMESH-BOT
